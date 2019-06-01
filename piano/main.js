6b53e90a828fa29b652a7008917fc4be1c5d0c5a
JenyaLoyowskiy <47608994+JenyaLoyowskiy@users.noreply.github.com>
2019-06-01 21:49:22 +0300

    Update main.js

--- piano/main.js
+++ piano/main.js
@@ -10,10 +10,14 @@ const pianoKey = [[65, 1],[83, 2],[68, 3],[70, 4],[71, 5],[72, 6],[74, 7],[75, 8
 
 window.addEventListener('keydown', ({keyCode}) => {
   for (let i = 0; i < pianoKey.length; i++) {
-    let item  = pianoKey[i];
-    if (item[0] == keyCode) {
-      let sound = new Audio('piano-master/'+item[1]+'.mp3');
+    let keyNumber  = pianoKey[i];
+    if (keyNumber[0] == keyCode) {
+      let sound = new Audio('piano-master/'+keyNumber[1]+'.mp3');
+      document.getElementById(keyNumber[1]).classList.add('red');
       sound.play();
+      setTimeout(()=>{
+      document.getElementById(keyNumber[1]).classList.remove('red');
+    },100)
     }
   }
 });
